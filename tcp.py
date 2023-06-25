467b369b2883915ef94e2b0c9586c277d3cedb9d
rad-silva <radsilva@outlook.com.br>
2023-06-25 12:50:00 -0300

    nova conexao e recebimento de segmentos

--- tcp.py
+++ tcp.py
@@ -18,12 +18,21 @@ class Servidor:
         self.callback = callback
 
     def _rdt_rcv(self, src_addr, dst_addr, segment):
-        src_port, dst_port, seq_no, ack_no, \
-            flags, window_size, checksum, urg_ptr = read_header(segment)
+        (
+            src_port,
+            dst_port,
+            seq_no,
+            ack_no,
+            flags,
+            window_size,
+            checksum,
+            urg_ptr
+        ) = read_header(segment)
 
         if dst_port != self.porta:
             # Ignora segmentos que não são destinados à porta do nosso servidor
             return
+    
         if not self.rede.ignore_checksum and calc_checksum(segment, src_addr, dst_addr) != 0:
             print('descartando segmento com checksum incorreto')
             return
@@ -33,25 +42,38 @@ class Servidor:
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
-            # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
-            # fazer aqui mesmo ou dentro da classe Conexao.
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
+            ack_no = seq_no + 1
+
+
+            handshake_header = fix_checksum(
+                make_header(dst_port, src_port, seq_no, ack_no, FLAGS_SYN + FLAGS_ACK),
+                dst_addr,
+                src_addr,
+            )
+            self.rede.enviar(handshake_header, src_addr)
+
             if self.callback:
                 self.callback(conexao)
+    
         elif id_conexao in self.conexoes:
             # Passa para a conexão adequada se ela já estiver estabelecida
             self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
+
         else:
             print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
                   (src_addr, src_port, dst_addr, dst_port))
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seq_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
+        self.seq_no = seq_no
+        self.ack_no = seq_no + 1
+        self.buffer = b''
+        self.timer_rodando = False
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
 
@@ -59,11 +81,30 @@ class Conexao:
         # Esta função é só um exemplo e pode ser removida
         print('Este é um exemplo de como fazer um timer')
 
+    # Trata o recebimento de segmentos provenientes da camada de rede.
+    # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
+    # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
-        # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
-        # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
-        # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
         print('recebido payload: %r' % payload)
+
+        if seq_no != self.ack_no:
+            print("segmento com seq_no errado")
+            return
+        
+        # else:
+        (src_addr, src_port, dst_addr, dst_port) = self.id_conexao
+        self.callback(self, payload)
+        self.ack_no += len(payload)
+        
+        handshake_header = fix_checksum(
+            make_header(dst_port, src_port, ack_no, self.ack_no, FLAGS_ACK),
+            dst_addr,
+            src_addr,
+        )
+        self.servidor.rede.enviar(handshake_header, src_addr)
+
+        
+
 
     # Os métodos abaixo fazem parte da API
 
@@ -78,9 +119,9 @@ class Conexao:
         """
         Usado pela camada de aplicação para enviar dados
         """
-        # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
+        
         pass
 
     def fechar(self):
